6530a17e217477ff59570b519d30e32519b05f6e
Vitor Cassiano <vitor.cassiano@olxbr.com>
2020-06-18 16:38:20 -0300

    fix test dependencie

--- kong_config_builder/password.py
+++ kong_config_builder/password.py
@@ -5,5 +5,5 @@ import random
 class PasswordManager:
     @staticmethod
     def generate(size: int) -> str:
-        password = f"{string.ascii_letters}{string.digits}"
+        password = string.ascii_letters + string.digits
         return "".join(random.choice(password) for i in range(int(size)))
